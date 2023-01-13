5931ba0cbabf70df6ffd426211a04092b80b83e9
changmin <dlckdals1004@naver.com>
2023-01-13 17:02:47 +0900

    react

--- React2022/my-app/src/App.js
+++ React2022/my-app/src/App.js
@@ -1,34 +1,103 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function Header() {
-  return <header>
-    <h1><a href="/">WEB</a></h1>
-  </header>
+function Header(props) {
+  console.log("props", props, props.title);
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
 }
 
-function Nav() {
-    return <nav>
-      <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">js</a></li>
-        </ol>
+function Nav(props) {
+  const lst = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lst.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(Number(event.target.id));
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lst}</ol>
     </nav>
+  );
 }
 
-function Article() {
-  return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
-  </article>
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
 }
 function App() {
+  // const _mode = useState("WELCOME");
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
+  ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>;
+  } else if (mode === "READ") {
+    let title,
+      body = null;
+    for (let i = 0; i < topics.length; i++) {
+      console.log(topics[i].id, id);
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
+  }
   return (
     <div className="App">
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          setMode("WELCOME");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
+        }}
+      ></Nav>
+      {content}
+      <a href="/create">Create</a>
     </div>
   );
 }
